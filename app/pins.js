4ae736a5827af277cd172443e614c63067a662a0
Farsinotare <info@farsinotare.com>
2014-10-14 22:16:17 +0200

    use pingraups for calculation; add tessel example

--- app/pins.js
+++ app/pins.js
@@ -1,19 +1,23 @@
+// pinboardjs
+// The pins place pins according to skin and
+// coordiantes from the layouter
+// 
+// (c) patrick mulder, 2014
+
 var _ = require('underscore');
 var Layouter = require('./layouter.js');
 
 Pins = function(s, options) {
 
+  // reference to canvas for drawing
   this.svg = s;
   this.board = options.board;
 
-  // basic paramters from skin
-  this.xOffset = this.board.skin.pins.offset.x;
-  this.yOffset = this.board.skin.pins.offset.y;
-  this.verticalOffset = this.board.skin.pins.offset.vertical;
+  // basic board paramters from skin
   this.height = this.board.skin.pins.height;
   this.inner_width = this.board.skin.board.middle.inner_width;
 
-  // pin render information
+  // pin render settings from skin 
   this.small = {};
   this.small.pinWidth = this.board.skin.pins.small.width;
   this.small.pinHeight = this.board.skin.pins.small.height;
@@ -26,14 +30,27 @@ Pins = function(s, options) {
   this.large.pinHeight = this.board.skin.pins.large.height;
   this.large.color = this.board.skin.pins.large.color;
 
+  this.processor = {};
+  this.processor.pinWidth = this.board.skin.processor.width;
+  this.processor.pinHeight = this.board.skin.processor.height;
+  this.processor.pinColor = this.board.skin.processor.height;
+
   this.initialize.apply(this, arguments);
 };
 
 // to override with custom initialization 
 Pins.prototype.initialize = function() {}
 
-Pins.prototype._renderPin = function(x, y, w, h, color) {
+Pins.prototype._renderPin = function(pin, skin) {
+
+  var x = pin.x;
+  var y = pin.y;
+  var w = skin.pinWidth;
+  var h = skin.pinHeight; 
+  var color = skin.color;
+
   r = this.svg.rect(x, y, w, h);
+
   r.attr({fill: color, stroke: '#000000'});
   r.click(function(e) {
     var node = e.currentTarget;
@@ -55,26 +72,27 @@ Pins.prototype.render = function() {
     rawPins: this.board,
     innerWidth: this.inner_width
   };
+
+  // instantiate a layouter
   var layouter = new Layouter(options);
   layouter.calcCoord();
   
   var pins = layouter.coords; 
 
   var that = this;
+
+  // draw pins
   pins.forEach(function(pin) {
-    console.log(pin.type);
+
     if (pin.type == 'large') {
-      that._renderPin(offsetX + pin.x, offsetY + pin.y,
-        that.large.pinWidth, that.large.pinHeight, that.large.color);
+      that._renderPin(pin, that.large); 
     } else if (pin.type == 'processor') {
-      that._renderPin(offsetX + pin.x, offsetY + pin.y,
-        260, that.large.pinHeight, that.large.color);
+      that._renderPin(pin, that.processor);
     } else {
-      that._renderPin(offsetX + pin.x, offsetY + pin.y,
-        that.small.pinWidth, that.small.pinHeight, that.small.color);
+      that._renderPin(pin, that.small);
     }
    
-    var label = s.text(offsetX + pin.x, offsetY + pin.y, pin.name);
+    var label = s.text(pin.x, pin.y - 5, pin.name);
     label.attr({fill: '#ffffff', stroke: 'none'});
   });
 }
